734b740d940ca477f29c76114841f8e67f87cdf8
Fredrik Josefsson <fredriiikjosefsson@gmail.com>
2016-04-07 20:43:21 +0200

    kan ej få färgen att byta..

--- scripts/test.js
+++ scripts/test.js
@@ -2,15 +2,24 @@ function doThisOnClick() {
 	alert("I do get it now!");
 }
 var originalColor;
-var doThisOnMouseEnter = function() {
-	textButton.style.color = "blue";
+function doThisOnMouseEnter(listElement) {
+	alert("hej");
+	listElement.style.backgroundColor = originalColor; //funkar inte
 }
 var doThisOnMouseOut = function() {
 	textButton.style.color = originalColor;
 }
-var textButton = document.getElementById("jsText");
+var textButton = document.getElementById("jsText3");
 var originalColor = textButton.style.color;
 textButton.style.cursor = "pointer";
 textButton.addEventListener("click", doThisOnClick);
 textButton.addEventListener("mouseenter", doThisOnMouseEnter);
 textButton.addEventListener("mouseleave", doThisOnMouseOut);
+
+var listElements = document.getElementById("subjectList").getElementsByTagName("li");
+for(var i = 0; i < listElements.length; i++){
+	alert(i);
+	listElements[i].addEventListener("click", function(){
+		doThisOnMouseEnter(listElements[i]);
+	});
+}
