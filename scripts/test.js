33b49bda5bd31ad6162383e1f9bcff872e75419e
Fredrik Josefsson <fredriiikjosefsson@gmail.com>
2016-04-07 18:33:01 +0200

    yee

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,15 @@
 function doThisOnClick() {
 	alert("I do get it now!");
 }
+var originalColor;
 var doThisOnMouseEnter = function() {
 	textButton.style.color = "blue";
 }
 var doThisOnMouseOut = function() {
-	textButton.style.color = "red";
+	textButton.style.color = originalColor;
 }
 var textButton = document.getElementById("jsText");
+var originalColor = textButton.style.color;
 textButton.style.cursor = "pointer";
 textButton.addEventListener("click", doThisOnClick);
 textButton.addEventListener("mouseenter", doThisOnMouseEnter);
