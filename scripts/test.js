4342f5b4c5eb8b0b9facd58860dc39dfb0373eb4
Fredrik Josefsson <Fredrik Josefsson>
2016-04-10 22:47:38 +0200

    yeay

--- scripts/test.js
+++ scripts/test.js
@@ -1,19 +1,10 @@
-function doThisOnClick() {
-	alert("I do get it now!");
-}
-var originalColor;
-function doThisOnMouseEnter(listElement) {
+function doThisOnClick(listElement) {
 	alert(listElement.target.innerHTML);
 }
-var doThisOnMouseOut = function() {
-	textButton.style.color = originalColor;
-}
-var textButton = document.getElementById("jsText3");
-var originalColor = textButton.style.color;
-textButton.style.cursor = "pointer";
 
 var list = document.getElementById("subjectList");
 var listElements = list.getElementsByTagName("li");
 for(var i = 0; i < listElements.length; i++){
-	listElements[i].addEventListener("click", doThisOnMouseEnter);
+	listElements[i].style.cursor = "pointer";
+	listElements[i].addEventListener("click", doThisOnClick);
 }
