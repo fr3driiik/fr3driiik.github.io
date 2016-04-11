e9554b81f06092f82fe89bddc6c7d529e95be5ce
Fredrik Josefsson <Fredrik Josefsson>
2016-04-11 14:09:39 +0200

    lots of stuffs

--- scripts/test.js
+++ scripts/test.js
@@ -2,9 +2,11 @@ function doThisOnClick(listElement) {
 	alert(listElement.target.innerHTML);
 }
 
-var list = document.getElementById("subjectList");
-var listElements = list.getElementsByTagName("li");
-for(var i = 0; i < listElements.length; i++){
-	listElements[i].style.cursor = "pointer";
-	listElements[i].addEventListener("click", doThisOnClick);
+var lists = document.getElementsByClassName("subjectList");
+for(var x = 0; x <  lists.length; x++){
+	var listElements = lists[x].getElementsByTagName("li");
+	for(var i = 0; i < listElements.length; i++){
+		listElements[i].style.cursor = "pointer";
+		listElements[i].addEventListener("click", doThisOnClick);
+	}
 }
