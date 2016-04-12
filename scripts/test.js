ad480610411ebbe7c8ab3eb89652f238a8728d6b
Fredrik Josefsson <Fredrik Josefsson>
2016-04-12 23:36:34 +0200

    media and effective events

--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,16 @@
 function doThisOnClick(listElement) {
-	alert(listElement.target.innerHTML);
+	if(listElement.currentTarget !== listElement.target){
+		alert(listElement.target.innerHTML);
+	}
+	listElement.stopPropagation();
 }
 
 var lists = document.getElementsByClassName("subjectList");
 for(var x = 0; x <  lists.length; x++){
+	lists[x].addEventListener("click", doThisOnClick);
+	
 	var listElements = lists[x].getElementsByTagName("li");
 	for(var i = 0; i < listElements.length; i++){
 		listElements[i].style.cursor = "pointer";
-		listElements[i].addEventListener("click", doThisOnClick);
 	}
 }
